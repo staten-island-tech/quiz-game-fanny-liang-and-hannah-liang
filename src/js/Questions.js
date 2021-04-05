8c9dc4243ac1258dc767e656e270de1f01c7c32e
fannyl6720 <fanny.liang23@sitechhs.com>
2021-04-04 21:58:17 -0400

    update questions.

--- src/js/Questions.js
+++ src/js/Questions.js
@@ -1,20 +1,56 @@
 const questions = [
-
-      //questions object{} should contain:
-    question: "What is the sports name?",
-    answersArr: [
     {
-        name: "sport", 
-        position: "1", 
-        content: "one", 
-        correct: true},
-    ],
-
-    question: "Who are the players involved in the sport?",
-    answersArr: [
+        question: "Amount of players that can be played on the field in soccer?",
+        contentA: 7,
+        contentB: 11,
+        contentC: 2,
+        contentD: 8,
+        correct: B
+    },
     {
-        name: "players", 
-        position: "2", 
-        content: "two", 
-        correct: true,}],
-    ]
+        question: "Amount of players that can be played on court in basketball",
+        contentA: 5,
+        contentB: 3,
+        contentC: 4,
+        contentD: 12,
+        correct: A
+    },
+    {
+        question: "Amount of players that can be played in tennis?",
+        contentA: 2,
+        contentB: 4,
+        contentC: neither,
+        contentD: both,
+        correct: D
+    },
+    {
+        question: "Amount of players that can be played on the field in baseball?",
+        contentA: 22,
+        contentB: 26,
+        contentC: 28,
+        contentD: 9,
+        correct: D
+    },
+    {
+        question: "Amount of players that can be played in volleyball?",
+        contentA: 5,
+        contentB: 6,
+        contentC: 7,
+        contentD: 8,
+        correct: B
+    },
+    {
+        question: "Amount of players that can be played on the field in football?",
+        contentA: 7,
+        contentB: 11,
+        contentC: 2,
+        contentD: 8,
+        correct: B
+    },
+],
+
+   function checkAnswer(answer){
+       if(answer == questions.correct){
+
+       }
+   } 
