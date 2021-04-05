a1088d30c91d0f30ccb8bc50e5d990e16b4e26b5
hannahliang11 <71453030+hannahliang11@users.noreply.github.com>
2021-04-04 20:34:47 -0400

    fixing error

--- src/js/Questions.js
+++ src/js/Questions.js
@@ -1,20 +1,26 @@
 const questions = [
-
-      //questions object{} should contain:
-    question: "What is the sports name?",
-    answersArr: [
+  //questions object{} should contain:
+  question,
+  "What is the sports name?",
+  answersArr,
+  [
     {
-        name: "sport", 
-        position: "1", 
-        content: "one", 
-        correct: true},
-    ],
+      name: "sport",
+      position: "1",
+      content: "one",
+      correct: true,
+    },
+  ],
 
-    question: "Who are the players involved in the sport?",
-    answersArr: [
+  question,
+  "Who are the players involved in the sport?",
+  answersArr,
+  [
     {
-        name: "players", 
-        position: "2", 
-        content: "two", 
-        correct: true,}],
-    ]
+      name: "players",
+      position: "2",
+      content: "two",
+      correct: true,
+    },
+  ],
+];
