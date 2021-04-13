8c6af10ada6a352c87ff6da4a8895f4127171ae7
fannyl6720 <fanny.liang23@sitechhs.com>
2021-04-12 23:26:54 -0400

    add questions

--- src/js/index.js
+++ src/js/index.js
@@ -97,6 +97,110 @@
   const quizContainer = document.getElementById("quiz");
   const resultsContainer = document.getElementById("results");
   const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {question: "Amount of players that can be played on the field in soccer?",
+    answers: {
+      a: "7",
+      b: "11",
+      c: "2",
+      d: "8",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Amount of players that can be played on court in basketball",
+    answers: {
+      a: "5",
+      b: "3",
+      c: "4",
+      d: "12",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Amount of players that can be played in tennis?",
+    answers: {
+      a: "2",
+      b: "4",
+      c: "neither",
+      d: "both",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "Amount of players that can be played on the field in baseball?",
+    answers: {
+      a: "10",
+      b: "11",
+      c: "18",
+      d: "9",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "Amount of players that can be played in volleyball?",
+    answers: {
+      a: "5",
+      b: "6",
+      c: "7",
+      d: "8",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Amount of players that can be played on the field in football?",
+    answers: {
+      a: "8",
+      b: "11",
+      c: "10",
+      d: "9",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question:
+      "Amount of players that can be played on the field in ice hockey?",
+    answers: {
+      a: "6",
+      b: "5",
+      c: "8",
+      d: "9",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Amount of players that can be played in badminton?",
+    answers: {
+      a: "4",
+      b: "2",
+      c: "neither",
+      d: "both",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question:
+      "Amount of players that can be played on the field in field hockey?",
+    answers: {
+      a: "11",
+      b: "10",
+      c: "13",
+      d: "9",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Amount of players that can be played on the field in rugby?",
+    answers: {
+      a: "16",
+      b: "15",
+      c: "19",
+      d: "18",
+    },
+    correctAnswer: "b",
+  },
+]
+
   buildQuiz();
 
   // Pagination
