bf6289c3053d5c0a8c3e5f2ec5a7fed19a577af5
hannahliang11 <71453030+hannahliang11@users.noreply.github.com>
2021-04-12 22:39:08 -0400

    working

--- src/js/DOMSelectors.js
+++ src/js/DOMSelectors.js
@@ -1,15 +1,14 @@
 //DOMSelectors Object
 
 const DOMSelectors = {
-    start: document.getElementById("start-btn"),
-    questions: document.getElementById("questions"),
-    scoreBoard : document.getElementById("scoreBoard"),
-    scoreTracker : document.getElementById("scoreTracker"),
-    Answers: document.getElementById("answer-buttons")
+  start: document.getElementById("start-btn"),
+  questions: document.getElementById("questions"),
+  scoreBoard: document.getElementById("scoreBoard"),
+  scoreTracker: document.getElementById("scoreTracker"),
+  Answers: document.getElementById("answer-buttons"),
 };
 
 console.log(DOMSelectors.begin);
 //start the game
 //begin on sound (alarm, whistle)
-
 export { DOMSelectors };
